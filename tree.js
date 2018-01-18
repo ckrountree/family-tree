36b220ebaaf2400bbd15bd4b4a4ff5370af69e68
ckrountree <jmail0207@gmail.com>
2018-01-18 10:26:05 -0800

    check accumulator array for existing parent and push children into array if parent exists there

--- tree.js
+++ tree.js
@@ -9,13 +9,16 @@ var people = [
 	{ name: 'Peter', parent: 'Alejandro' }
 ];
 
-const treeArr = people.reduce((accum, {name, parent}) => {
+let treeArr = people.reduce((accum, {name, parent}) => {
 	const childName = {name: name, children: []};
 	const parentName = {name: parent, children: [childName]};
 
-	if(accum.length === 0) accum.push(parentName);
+	function match(parentObj) {
+		parentObj.name === parent;
+	}
 
+	if(accum.length === 0) accum.push(parentName);
+	else if(accum.findIndex(match) !== -1) accum[accum.findIndex(match)].children.push(parentName);
 	return accum;
 }, []);
-
 console.log(treeArr);
