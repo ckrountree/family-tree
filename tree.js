a3cd2c6c1653f5b1c1fbdfc5cceffeef0c0447ab
ckrountree <jmail0207@gmail.com>
2018-01-18 09:39:03 -0800

    create tree.js and add initial dataset

---
+++ tree.js
@@ -0,0 +1,10 @@
+var people = [
+	{ name: 'John', parent: 'Sue' },
+	{ name: 'Jack', parent: 'Steve' },
+	{ name: 'Jill', parent: 'Sally' },
+	{ name: 'Wayne', parent: 'Jack' },
+	{ name: 'Twinkle', parent: 'Jack' },
+	{ name: 'Garth', parent: 'Sue' },
+	{ name: 'Steve', parent: 'Alejandro' },
+	{ name: 'Peter', parent: 'Alejandro' }
+];
