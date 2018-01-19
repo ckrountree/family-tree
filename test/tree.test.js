5f796f848326217a540b3ceb19c181833a5c6e78
ckrountree <jmail0207@gmail.com>
2018-01-19 14:08:24 -0800

    add to test

--- test/tree.test.js
+++ test/tree.test.js
@@ -1,9 +1,29 @@
 const chai = require('chai');
 const assert = chai.assert;
 
-describe('Array', function() {
-	it('should start with an empty array', function() {
+describe('Array', () => {
+	it('should start with an empty array', () => {
 		const accum = [];
 		assert.equal(accum.length, 0);
 	});
+
+	it('should add a key:value object if array is empty', () => {
+		const people = [
+			{ name: 'John', parent: 'Sue' }
+		];
+		const parentName = {name: parent, children: []};
+
+		people.reduce((accum, {name, parent}) => {
+			if(accum.length === 0) accum.push(parentName);
+		},
+		assert.equal(accum.length === 0, { name: 'John', parent: 'Sue' });
+	});
+
+	// it('should check that parent name is in array', () => {
+
+	// });
+
+	// it('should check that parent name is not in array', () => {
+
+	// });
 });
